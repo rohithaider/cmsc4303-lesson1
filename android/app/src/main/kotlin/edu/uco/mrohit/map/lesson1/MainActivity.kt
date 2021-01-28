96c67f7736fc1db47c3af9304cd5198742e3913f
Mohtashim <rohithaider@gmail.com>
2021-01-28 16:54:26 -0600

    initial commit

---
+++ android/app/src/main/kotlin/edu/uco/mrohit/map/lesson1/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.mrohit.map.lesson1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(Lesson1App());
+
+}
+
+class Lesson1App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Lesson1 App')
+    );
+  }
+
+
+
+}
