5ca576c6a2e4a34624b518d9bb1ad5a0ca3fbd0f
Mohtashim <rohithaider@gmail.com>
2021-01-28 20:00:44 -0600

    body part

--- lib/screen/start_screen.dart
+++ lib/screen/start_screen.dart
@@ -1,16 +1,43 @@
-
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget{
-  static const routeName ='/startScreen';
+class StartScreen extends StatelessWidget {
+  static const routeName = '/startScreen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Start Menu'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.alarm), //passing icon object
+              onPressed: () {
+                //using annoynimous function
+                print('Alarm');
+              }),
+          IconButton(
+              icon: Icon(Icons.message_sharp),
+              onPressed: () {
+                print('Message');
+              })
+        ],
       ),
-        body: Text('Welcome to the Start Screen'),
+      body: Column(
+        children: [
+          RaisedButton(
+            onPressed: () {
+              print('Menu 1');
+            },
+            child: Text('Menu 1'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              print('Menu 2');
+            },
+            child: Text('Menu 2'),
+          ),
+          Text('third menu')
+        ],
+      ),
     );
   }
-
 }
