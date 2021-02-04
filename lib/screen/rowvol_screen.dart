0749ce13218e91031e96b60dd31a065e04e9d744
Mohtashim <rohithaider@gmail.com>
2021-02-04 15:59:48 -0600

    single child scroll view

--- lib/screen/rowvol_screen.dart
+++ lib/screen/rowvol_screen.dart
@@ -8,7 +8,94 @@ class RowColScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Row/Column Layout Demo'),
       ),
-      body: Text('Row Col'),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 1',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 2',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 3',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 1',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 1',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 2',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 3',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 1',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 1',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 2',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 3',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            color: Colors.teal[900],
+            child: Text(
+              'Row 1',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
