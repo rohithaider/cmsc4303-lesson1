911d6463552f8a335a407213439814612c719449
Mohtashim <rohithaider@gmail.com>
2021-02-04 18:45:15 -0600

    flex in a row

--- lib/screen/expandedwidget_screen.dart
+++ lib/screen/expandedwidget_screen.dart
@@ -8,7 +8,32 @@ class ExpandedWidgetScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Expanded widget"),
       ),
-      body: Text('Expanded widget'),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.teal[700],
+                  child: Text('A', style: Theme.of(context).textTheme.headline6),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Image.asset('images/download.jpg'),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.red[700],
+                  child: Text('B', style: Theme.of(context).textTheme.headline6),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
