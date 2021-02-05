f8c4c97f619663ae0262df8853b412d89a8404d2
Mohtashim <rohithaider@gmail.com>
2021-02-04 20:49:38 -0600

    complete business card

--- lib/screen/businesscard_screen.dart
+++ lib/screen/businesscard_screen.dart
@@ -10,45 +10,61 @@ class BusinessCardScreen extends StatelessWidget {
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: ClipOval(
-                child: Image.asset(
-                  'images/business.jpg',
-                  height: 200,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 50, 30, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: ClipOval(
+                    child: Image.asset(
+                      'images/business.jpg',
+                      height: 200,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Text(
-              'Name:',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Text('Mohtashim Rohit', style: Theme.of(context).textTheme.headline4),
-            Text(
-              'Title:',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Text('Student', style: Theme.of(context).textTheme.headline4),
-            Text(
-              'Major:',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Text('Computer Science', style: Theme.of(context).textTheme.headline4),
-            Row(
-              children: [
-                Icon(Icons.email),
-                Text('mrohit@uco.edu', style: Theme.of(context).textTheme.headline5),
+                Divider(
+                  height: 40,
+                  color: Colors.yellow,
+                ),
+                Text(
+                  'Name:',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Text('Mohtashim Rohit', style: Theme.of(context).textTheme.headline4),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Title:',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Text('Student', style: Theme.of(context).textTheme.headline4),
+                SizedBox(height: 20),
+                Text(
+                  'Major:',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Text('Computer Science', style: Theme.of(context).textTheme.headline4),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Icon(Icons.email),
+                    SizedBox(width: 30),
+                    Text('mrohit@uco.edu', style: Theme.of(context).textTheme.headline5),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.phone),
+                    SizedBox(width: 30),
+                    Text('405-850-1170', style: Theme.of(context).textTheme.headline5),
+                  ],
+                )
               ],
             ),
-            Row(
-              children: [
-                Icon(Icons.phone),
-                Text('405-850-1170', style: Theme.of(context).textTheme.headline5),
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
