6758725a4e8e56e6ac4b2b0f76e90781ff046335
Mohtashim <rohithaider@gmail.com>
2021-02-04 20:04:40 -0600

    business card

--- lib/screen/businesscard_screen.dart
+++ lib/screen/businesscard_screen.dart
@@ -8,7 +8,49 @@ class BusinessCardScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('My Business Card'),
       ),
-      body: Text('Business Card demo'),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: ClipOval(
+                child: Image.asset(
+                  'images/business.jpg',
+                  height: 200,
+                ),
+              ),
+            ),
+            Text(
+              'Name:',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text('Mohtashim Rohit', style: Theme.of(context).textTheme.headline4),
+            Text(
+              'Title:',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text('Student', style: Theme.of(context).textTheme.headline4),
+            Text(
+              'Major:',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text('Computer Science', style: Theme.of(context).textTheme.headline4),
+            Row(
+              children: [
+                Icon(Icons.email),
+                Text('mrohit@uco.edu', style: Theme.of(context).textTheme.headline5),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.phone),
+                Text('405-850-1170', style: Theme.of(context).textTheme.headline5),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
